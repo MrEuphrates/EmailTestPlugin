6c3a29c363ccf16e14f91922cffe5357b81bd0ba
MrEuphrates <rhunter1517@gmail.com>
2016-06-09 12:57:51 -0400

    Testing making changes
    I uncommented a use of message box to test committing and eventually
    synchronizing/pulling.

--- EmailTestPlugin/Plugin.cs
+++ EmailTestPlugin/Plugin.cs
@@ -43,10 +43,10 @@ namespace EmailTestPlugin
             Act.UI.Correspondence.EmailMailMergeInfo emmi = new Act.UI.Correspondence.EmailMailMergeInfo();
             //TODO Email Field Name List is the list of physical field names that have the Email data type - might be usable for the mail merge
             var results = emmi.EmailFieldNameList(application);
-            //foreach (string s in results)
-            //{
-            //    MessageBox.Show(s);
-            //}
+            foreach (string s in results)
+            {
+                MessageBox.Show(s);
+            }
             //MessageBox.Show(application.ApplicationState.CurrentContact.FullName);
             //Act.UI.Correspondence.UICorrespondenceManager uicm = application.UICorrespondenceManager;
             //uicm.WriteEmail(application.ApplicationState.CurrentContact, results);
